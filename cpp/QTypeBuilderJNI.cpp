5252c2e38934a75c53f871718f9ab1218c48ea69
Frank Vernaillen <frank.vernaillen@vib.be>
2016-11-08 16:52:41 +0100

    Add some skeleton code for the Quasar type builder. Not functional yet.

---
+++ cpp/QTypeBuilderJNI.cpp
@@ -0,0 +1,35 @@
+
+#include "be_vib_bits_QTypeBuilder.h"
+
+#include "quasar_dsl.h"
+
+#include "WideString.h"
+
+#include <iostream>
+
+using namespace quasar;
+
+extern jfieldID qvalue_ptr_fieldID;
+
+JNIEXPORT jlong JNICALL Java_be_vib_bits_QTypeBuilder_newQTypeBuilder(JNIEnv* env, jclass, jstring moduleName, jstring typeName)
+{
+	WideString moduleNameW(env, moduleName);
+	WideString typeNameW(env, typeName);
+	TypeBuilder* tb = new TypeBuilder(moduleNameW, typeNameW);
+	return reinterpret_cast<jlong>(tb);
+}
+
+JNIEXPORT void JNICALL Java_be_vib_bits_QTypeBuilder_addField(JNIEnv* env, jobject obj, jstring fieldName, jobject fieldType)
+{
+	WideString fieldNameW(env, fieldName);
+
+	jlong typeBuilderPtr = env->GetLongField(obj, qvalue_ptr_fieldID);
+	assert(typeBuilderPtr != 0);
+	TypeBuilder* tb = reinterpret_cast<TypeBuilder*>(typeBuilderPtr);
+
+	jlong typePtr = env->GetLongField(fieldType, qvalue_ptr_fieldID);
+	assert(typePtr != 0);
+	Type* t = reinterpret_cast<Type*>(typePtr);
+
+	tb->AddField(fieldNameW, *t);
+}
---
+++ java/src/be/vib/bits/QTypeBuilder.java
@@ -0,0 +1,16 @@
+package be.vib.bits;
+
+public class QTypeBuilder extends QValue
+{
+	public QTypeBuilder(String moduleName, String typeName)
+	{
+		super();
+		ptr = newQTypeBuilder(moduleName, typeName);
+	}
+	
+	public native void addField(String fieldName, QType fieldType);
+	
+	private native static long newQTypeBuilder(String moduleName, String typeName);
+	
+	// TODO: QType createType...()   -- hmm, this will be somewhat more fun
+}
