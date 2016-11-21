755366dc798345afa145f156a79d32c7901d06e6
Frank Vernaillen <frank.vernaillen@vib.be>
2016-11-21 21:54:12 +0100

    Remove debug print statements.

--- java/src/be/vib/bits/QUtils.java
+++ java/src/be/vib/bits/QUtils.java
@@ -4,12 +4,7 @@ public class QUtils
 {
 	public static QValue newCubeFromGrayscaleArray(int width, int height, byte[] pixels)
 	{
-		System.out.println("newCubeFromGrayscaleArray (Java thread=" + Thread.currentThread().getId() + ")");
-		System.out.println("w=" + width + " h=" + height + " pixels=" + pixels);
-
 		long p = newCubeFromGrayscaleArrayNative(width, height, pixels);
-		
-		System.out.println("got QValue ptr back from native code.");
 		return new QValue(new QNativePointer(p));
 	}
 	public native static byte[] newGrayscaleArrayFromCube(int width, int height, QValue cube);	
