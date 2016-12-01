820f3cf6e4a70b7b06e3704dbe3ee5032ca7c7d3
Frank Vernaillen <frank.vernaillen@vib.be>
2016-12-01 10:40:43 +0100

    Minor change to comment.

--- java/src/be/vib/bits/QFunction.java
+++ java/src/be/vib/bits/QFunction.java
@@ -11,11 +11,17 @@ public class QFunction extends QValue
 	// Note: the maximum supported number of args is 8.
 	// If you pass more arguments to apply() it will throw an IllegalArgumentException. 
 	//
-	// TODO:
-	// use apply(Object...) instead of apply(QValue...) so we can call use this syntax:
+	// TODO?
+	// We could add some syntactic sugar and define an
+	//    apply(Object...)
+	// instead of
+	//    apply(QValue...)
+	// so we can use this syntax:
 	//    f.apply(1, 2.0f, "abc");
 	// instead of
 	//    f.apply(new QValue(1), new QValue(2.0f), new QValue("abc"))
+	//
+	// It would make the QFunction JNI code more complex though. Also a bit slower?
 	//
 	public QValue apply(QValue... args) throws java.lang.IllegalArgumentException
 	{
