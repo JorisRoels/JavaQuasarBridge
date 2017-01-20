134d3dcba15c1147d2f1c7932d6cafee26528a41
Frank Vernaillen <frank.vernaillen@vib.be>
2017-01-20 17:48:33 +0100

    When running the tests, make sure to use a 64 bit Java runtime.

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-java -enableassertions -cp output\class -Djava.library.path=.\output Test
+"e:\Program Files\Java\jdk1.8.0_92\bin\java.exe" -enableassertions -cp output\class -Djava.library.path=.\output Test
 @echo note: bridge was run with assertions enabled
 
 
