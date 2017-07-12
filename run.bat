c44d60d1afbb9caabc104f8b12838dddab390405
Frank Vernaillen <frank.vernaillen@vib.be>
2017-07-12 13:01:26 +0200

    Fixed broken script to run the tests.

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-"e:\Program Files\Java\jdk1.8.0_92\bin\java.exe" -enableassertions -cp dist\JavaQuasarBridge.jar  -Djava.library.path=.\dist Test
+"e:\Program Files\Java\jdk1.8.0_92\bin\java.exe" -enableassertions -cp dist\JavaQuasarBridge-0.0.1.jar  -Djava.library.path=.\dist Test
 @rem The java.library.path is only needed so java can find the dll for System.loadLibrary, since that is also located in the "dist" folder
 @rem This can be removed once we add the dll to the jar, and extract it at runtime.
 @echo note: bridge was run with assertions enabled
